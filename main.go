9fa3acf8172c3bc7b3d203414b728a972f5f765a
GoremanIX <jansen.arthan@gmail.com>
2018-04-24 16:48:38 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"os/signal"
+	"runtime"
+	"strings"
+	"syscall"
+
+	dg "github.com/bwmarrin/discordgo"
+)
+
+// Config : the structure of the config file
+type Config struct {
+	BotID string
+}
+
+var newMessageInstructions = map[string]func(*dg.Session, *dg.MessageCreate){
+	"~headsortails": headsTails,
+}
+
+func headsTails(s *dg.Session, m *dg.MessageCreate) {
+	num := rand.Intn(2)
+	if num == 0 {
+		s.ChannelMessageSend(m.ChannelID, "Heads")
+	} else {
+		s.ChannelMessageSend(m.ChannelID, "Tails")
+	}
+}
+
+func swear() {
+
+}
+
+func main() {
+	var cfg Config
+	runtime.GOMAXPROCS(2)
+	readConfig(&cfg)
+
+	session, err := dg.New(cfg.BotID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer session.Close()
+
+	if err = session.Open(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	session.AddHandler(newMessage)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, syscall.SIGSEGV, syscall.SIGHUP)
+	fmt.Println("Setup Complete")
+	<-sc
+}
+
+func readConfig(cfg *Config) {
+	raw, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	json.Unmarshal(raw, cfg)
+}
+
+func newMessage(s *dg.Session, m *dg.MessageCreate) {
+
+	keyWord := strings.Split(m.Content, " ")[0]
+	if val, ok := newMessageInstructions[keyWord]; ok {
+		val(s, m)
+	}
+}
